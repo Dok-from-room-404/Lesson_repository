e62ff4c64f6dea83237402bd438879241e5561fa
Room 404 <dbuhanov366@gmail.com>
2022-12-20 20:53:47 +0900

    Update Modle.py

--- Modle.py
+++ Modle.py
@@ -12,7 +12,8 @@ class Board:
         self.width = width
         self.height = height
         self.cell_size = cell_size
-        self.board = [[0] * width for _ in range(height)]
+        self.not_go_board = []
+        self.actor_pos = []
         self.left = 0
         self.top = 0
         self.dic_image_from_level = dic_image_from_level
@@ -24,45 +25,78 @@ class Board:
         wcolor = pygame.Color(255, 255, 255)
         for y in range(self.height):
             for x in range(self.width):
-                n_x, n_y = self.left + self.cell_size * y, self.top + self.cell_size * x
+                n_x, n_y = self.left + self.cell_size * x, self.top + self.cell_size * y
                 k_x, k_y = self.cell_size, self.cell_size
                 
                 pygame.draw.rect(screen, wcolor, (n_x, n_y, k_x, k_y), 1)
-                
-                
+
                 level_res = level[y][x]
-                print(level_res)
+                
                 if level_res == "@":
                     titleRect = self.dic_image_from_level["."].get_rect()
                     titleRect.bottomleft = (n_x, n_y + 50)
+                    self.actor_pos = [n_x, n_y + 50]
                     screen.blit(self.dic_image_from_level["."], titleRect)
+                    
+                    
+                if level_res == "#":
+                    self.not_go_board.append([n_x, n_y + 50])
  
-                    
-                    
-                    
-                    
                 titleRect = self.dic_image_from_level[level[y][x]].get_rect()
                 titleRect.bottomleft = (n_x, n_y + 50)
                 
                 screen.blit(self.dic_image_from_level[level[y][x]], titleRect)
+        
+        
+    def chek(self, res):
+        return res not in self.not_go_board and (res[0] > 0 and res[1] > 0)
+        
+        
+        
+    def last_pos_actor(self, screen): 
+            titleRect = self.dic_image_from_level["."].get_rect()
+            titleRect.bottomleft = self.actor_pos
+            
+            screen.blit(self.dic_image_from_level["."], titleRect)
+            
+    def go(self, screen, res):
+            titleRect = self.dic_image_from_level["@"].get_rect()
+            self.actor_pos = res
+            titleRect.bottomleft = self.actor_pos
+            screen.blit(self.dic_image_from_level["@"], titleRect)
+                
+                
+    def go_left(self, screen): 
+        res = [self.actor_pos[0] - 50, self.actor_pos[1]]
+        if self.chek(res):
+            self.last_pos_actor(screen)
+            self.go(screen, res)
+        
+    
+    def go_right(self, screen): 
+        res = [self.actor_pos[0] + 50, self.actor_pos[1]]
+        if self.chek(res):
+            self.last_pos_actor(screen)
+            self.go(screen, res)
+
+        
+        
+        
+    
+    def go_up(self, screen): 
+        res = [self.actor_pos[0], self.actor_pos[1] - 50]
+        if self.chek(res):
+            self.last_pos_actor(screen)
+            self.go(screen, res)
+
+    
+    def go_down(self, screen): 
+        res = [self.actor_pos[0], self.actor_pos[1] + 50]
+        if self.chek(res):
+            self.last_pos_actor(screen)
+            self.go(screen, res)
+
  
     
  
-    def get_cell(self, mouse_pos):
-        board_width = self.width * self.cell_size
-        board_height = self.height * self.cell_size
-        if self.left < mouse_pos[0] < self.left + board_width:
-            if self.top < mouse_pos[1] < self.top + board_height:
-                cell_coords = (mouse_pos[1] - self.left) // self.cell_size,\
-                              (mouse_pos[0] - self.top) // self.cell_size
-                return cell_coords
-        return None
- 
-    def click(self, cell_coords):
-        x = (cell_coords[0] // self.cell_size) - (self.left // self.cell_size)
-        y = (cell_coords[1] // self.cell_size) - (self.top // self.cell_size)
-        
-        
-        if x >= 0 and y >= 0 and x < self.width and y < self.height:
-            return (x, y)
-        
+
